b0d31d8489073e3da0f6dfcf13f15098fc813af6
bryantt23 <bryantt23@gmail.com>
2015-07-03 02:33:13 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  root 'posts#index'
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+    resources :posts
+end
